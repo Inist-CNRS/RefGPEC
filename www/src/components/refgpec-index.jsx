7e2b36f83a6e6aa0392c9f282c7d622e614e4bb2
Stéphane Gully <stephane.gully@gmail.com>
2017-01-09 18:35:19 +0100

    adjust index texts

--- www/src/components/refgpec-index.jsx
+++ www/src/components/refgpec-index.jsx
@@ -20,16 +20,16 @@ module.exports = React.createClass({
 
               <div className="jumbotron">
                 <div className="container">
-                  <h1>Une démarche GPEC avec RefGPEC</h1>
+                  <h1>Mener sa démarche GPEC<br/> avec RefGPEC</h1>
                   <p>
                   <img className="gpec-big-logo img-responsive pull-left" src="/img/gpec_256x256.png" alt="" />
-                                  L'outil RefGPEC permet la mise en place d'un <a data-toggle="tab" className="nav-link" href="#skills" onClick={this.handleNavigateTab}>référentiel des compétences</a> et d'un <a data-toggle="tab" className="nav-link" href="#profils" onClick={this.handleNavigateTab}>référentiel des profils de poste</a> de votre organisation.
+                                  L'outil RefGPEC permet la mise en place numérique d'un <a data-toggle="tab" className="nav-link" href="#skills" onClick={this.handleNavigateTab}>référentiel des compétences</a> et d'un <a data-toggle="tab" className="nav-link" href="#profils" onClick={this.handleNavigateTab}>référentiel des profils de poste</a> de votre organisation.
                   </p>
                   <p>
-                                  Ces <a data-toggle="tab" className="nav-link" href="#profils-skills" onClick={this.handleNavigateTab}>compétences peuvent ensuite être associées à chacuns des profils référencés</a> en précisant une <a data-toggle="tab" className="nav-link" href="#levels" onClick={this.handleNavigateTab}>modulation</a> afin d'obtenir un état des lieux précis des compétences nécessaires pour exercer les différents postes de manière optimale.
+                                  Ces <a data-toggle="tab" className="nav-link" href="#profils-skills" onClick={this.handleNavigateTab}>compétences peuvent ensuite être associées à chacuns des profils référencés</a> en précisant une <a data-toggle="tab" className="nav-link" href="#levels" onClick={this.handleNavigateTab}>modulation</a> afin d'obtenir un état des lieux précis des compétences nécessaires pour exercer de manière optimale les différents postes au sein de votre organisation.
                   </p>
                   <p>
-                                  A noter que le recensement des compétences effectives des personnes exercant les postes en question n'entre pas dans le périmètre de RefGPEC.
+                                  A noter que l'évaluation des compétences effectives des personnes exercant les postes en question n'entre pas dans le périmètre de RefGPEC.
                   </p>
                 </div>
               </div>
