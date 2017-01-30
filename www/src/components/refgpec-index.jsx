e0eed3f97719b2c1c87d6c17dc5464a8f76092e8
Stéphane Gully <stephane.gully@gmail.com>
2017-01-30 15:17:47 +0100

    fix ortho

--- www/src/components/refgpec-index.jsx
+++ www/src/components/refgpec-index.jsx
@@ -26,7 +26,7 @@ module.exports = React.createClass({
                                   L'outil RefGPEC permet la mise en place numérique d'un <a data-toggle="tab" className="nav-link" href="#skills" onClick={this.handleNavigateTab}>référentiel des compétences</a> et d'un <a data-toggle="tab" className="nav-link" href="#profils" onClick={this.handleNavigateTab}>référentiel des profils de poste</a> de votre organisation.
                   </p>
                   <p>
-                                  Ces <a data-toggle="tab" className="nav-link" href="#profils-skills" onClick={this.handleNavigateTab}>compétences peuvent ensuite être associées à chacuns des profils référencés</a> en précisant une <a data-toggle="tab" className="nav-link" href="#levels" onClick={this.handleNavigateTab}>modulation</a> afin d'obtenir un état des lieux précis des compétences nécessaires pour exercer de manière optimale les différents postes au sein de votre organisation.
+                                  Ces <a data-toggle="tab" className="nav-link" href="#profils-skills" onClick={this.handleNavigateTab}>compétences peuvent ensuite être associées à chacun des profils référencés</a> en précisant une <a data-toggle="tab" className="nav-link" href="#levels" onClick={this.handleNavigateTab}>modulation</a> afin d'obtenir un état des lieux précis des compétences nécessaires pour exercer de manière optimale les différents postes au sein de votre organisation.
                   </p>
                   <p>
                                   A noter que l'évaluation des compétences effectives des personnes exercant les postes en question n'entre pas dans le périmètre de RefGPEC.
