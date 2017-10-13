864c8b30b318b0fbeb1c4ad6ad3389d3b090c546
Jimmy Falck <jimmy.falck@inist.fr>
2017-10-13 10:15:45 +0200

    insert data into types, domains & skills

---
+++ sql/009-insert-types.sql
@@ -0,0 +1,5 @@
+INSERT INTO skills_types (st_code,st_shortname)
+ VALUES
+ ('sf','savoir-faire'),
+ ('se','savoir-être'),
+ ('s','savoir');
---
+++ sql/011-insert-skills.sql
@@ -0,0 +1,6 @@
+INSERT INTO skills (skill_code,skill_shortname,skill_free_comments,sd_code,st_code)
+ VALUES
+ ('c-s-lang-1','Anglais','Anglais, écrit, oral, tout confondu','lang','s'),
+ ('c-sf-info-1','Langage de programmation','','info','sf'),
+ ('c-sf-info-2','Elasticsearch','','info','sf'),
+ ('c-sf-geadmin-1','Elaboration et suivi budgétaire','','geadmin','sf');
