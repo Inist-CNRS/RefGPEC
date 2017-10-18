09cc38216d15a84df28905871b037eca321d3fd2
Stéphane Gully <stephane.gully@gmail.com>
2017-10-18 22:46:53 +0200

    inistcnrs/refgpec-db docker image is now able to load new sql file found in /docker-entrypoint.initdb2.d/ folder

--- refgpec-db/docker-entrypoint.overload.sh
+++ refgpec-db/docker-entrypoint.overload.sh
@@ -2,9 +2,13 @@
 
 # inject config.json parameters to env
 # only if not already defined in env
-export POSTGRES_DB=${POSTGRES_DB:=$(jq -r .POSTGRES_DB /config.json)}
-export POSTGRES_USER=${POSTGRES_USER:=$(jq -r .POSTGRES_USER /config.json)}
-export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:=$(jq -r .POSTGRES_PASSWORD /config.json)}
-export PGDATA=${PGDATA:=$(jq -r .PGDATA /config.json)}
+export POSTGRES_DB=${POSTGRES_DB:=$(jq -r -M .POSTGRES_DB /config.json | grep -v null)}
+export POSTGRES_USER=${POSTGRES_USER:=$(jq -r -M .POSTGRES_USER /config.json | grep -v null)}
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:=$(jq -r -M .POSTGRES_PASSWORD /config.json | grep -v null)}
+export PGDATA=${PGDATA:=$(jq -r -M .PGDATA /config.json | grep -v null)}
 
+# load /docker-entrypoint.initdb2.d/ sql files sin background
+docker-entrypoint.initdb2.d-loader.sh &
+
+# start postgresql
 exec /usr/local/bin/docker-entrypoint.sh $@
