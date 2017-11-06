4f34ac3cc9bab10095a84fc64a69ed5ca91281f2
Stéphane Gully <stephane.gully@gmail.com>
2017-11-06 15:18:03 +0100

    Version 1.4.1

--- refgpec-front/src/components/refgpec-home.jsx
+++ refgpec-front/src/components/refgpec-home.jsx
@@ -205,7 +205,7 @@ var RefGpecHome = createReactClass({
         <div className="col-lg-12">
           <p className="muted pull-right">
             <span className="fa fa-github"></span> &nbsp;
-            <a href="https://github.com/Inist-CNRS/refgpec">RefGPEC</a> version 1.4.0
+            <a href="https://github.com/Inist-CNRS/refgpec">RefGPEC</a> version 1.4.1
           </p>
         </div>
       </footer>
