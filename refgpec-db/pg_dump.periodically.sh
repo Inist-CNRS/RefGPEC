af5b21b44ccbadb4b2698a439fb29a26b645512c
Stéphane Gully <stephane.gully@gmail.com>
2017-11-20 18:04:20 +0100

    add the PGDUMP_ALLOWED_HOUR parameter in order to tell the database to backup just at a specific hour

--- refgpec-db/pg_dump.periodically.sh
+++ refgpec-db/pg_dump.periodically.sh
@@ -14,6 +14,22 @@ do
     echo "SELECT datname FROM pg_database"  | psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname $POSTGRES_DB 2>/dev/null 1>/dev/null
     ret=$?
   done
+  echo ""
+
+  # loop until the allow hour to run the backup is reached
+  echo -n "Waiting until the backup hour (${PGDUMP_ALLOWED_HOUR}) is ok to run pg_dump."
+  ret=1
+  while [ $ret -gt 0 ]
+  do
+    echo -n "."
+    if [ "${PGDUMP_ALLOWED_HOUR}" == "$(date '+%H')" ]; then
+      echo ""
+      echo "It is $(date '+%Y-%m-%d %H:%M:%S') so ${PGDUMP_ALLOWED_HOUR}h is now so we can run pg_dump."
+      ret=0
+    else
+      sleep 10m
+    fi
+  done
 
   BACKUP_FILE=/ezmaster-data/sqldump/dump.$(date '+%Y-%m-%d_%Hh%M').sql
   echo "Running backup through pg_dump: $BACKUP_FILE"
