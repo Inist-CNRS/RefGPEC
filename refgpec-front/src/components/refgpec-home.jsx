7e3290c18be4ef9c0b67c03763ecb030a4e448d1
jimmojito <jimmy.falck@inist.fr>
2017-12-04 11:38:38 +0100

    Version 1.8.0

--- refgpec-front/src/components/refgpec-home.jsx
+++ refgpec-front/src/components/refgpec-home.jsx
@@ -253,7 +253,7 @@ var RefGpecHome = createReactClass({
             <p className="muted pull-right">
               <span className="fa fa-github" /> &nbsp;
               <a href="https://github.com/Inist-CNRS/refgpec">RefGPEC</a>{" "}
-              version 1.7.0
+              version 1.8.0
             </p>
           </div>
         </footer>
