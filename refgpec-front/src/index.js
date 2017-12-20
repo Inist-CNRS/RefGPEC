4fff0f6c440895348be80e96c8b061b36390c746
jimmojito <jimmy.falck@inist.fr>
2017-12-20 13:34:27 +0100

    check browser

--- refgpec-front/src/index.js
+++ refgpec-front/src/index.js
@@ -3,6 +3,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { Modal } from "react-bootstrap";
 import $ from "jquery";
 
 import "font-awesome/css/font-awesome.css";
@@ -18,30 +19,16 @@ import RefGpecSkillsDomainsModel from "./models/refgpec-skills-domains-model.js"
 import RefGpecLevelsModel from "./models/refgpec-levels-model.js";
 import RefGpecProfilsSkillsModel from "./models/refgpec-profils-skills-model.js";
 
-var modelOptions = {
+let modelOptions = {
   fakeLoadingMaxDelay: 500
   //fakeData: true
 };
-var levelsModel = new RefGpecLevelsModel(modelOptions);
-var skillsModel = new RefGpecSkillsModel(modelOptions);
-var skillsTypesModel = new RefGpecSkillsTypesModel(modelOptions);
-var skillsDomainsModel = new RefGpecSkillsDomainsModel(modelOptions);
-var profilsModel = new RefGpecProfilsModel(modelOptions);
-var profilsSkillsModel = new RefGpecProfilsSkillsModel(modelOptions);
-
-function render() {
-  ReactDOM.render(
-    <RefGpecHome
-      levelsModel={levelsModel}
-      skillsModel={skillsModel}
-      skillsTypesModel={skillsTypesModel}
-      skillsDomainsModel={skillsDomainsModel}
-      profilsModel={profilsModel}
-      profilsSkillsModel={profilsSkillsModel}
-    />,
-    document.getElementById("refgpec")
-  );
-}
+let levelsModel = new RefGpecLevelsModel(modelOptions);
+let skillsModel = new RefGpecSkillsModel(modelOptions);
+let skillsTypesModel = new RefGpecSkillsTypesModel(modelOptions);
+let skillsDomainsModel = new RefGpecSkillsDomainsModel(modelOptions);
+let profilsModel = new RefGpecProfilsModel(modelOptions);
+let profilsSkillsModel = new RefGpecProfilsSkillsModel(modelOptions);
 
 profilsModel.subscribe(render);
 skillsModel.subscribe(render);
@@ -50,6 +37,72 @@ skillsDomainsModel.subscribe(render);
 levelsModel.subscribe(render);
 profilsSkillsModel.subscribe(render);
 
+function render() {
+  if (detectIE()) {
+    return ReactDOM.render(
+      <Modal show>
+        <Modal.Header>
+          <Modal.Title>
+            <h2 className="modal-title text-center">
+              Internet Explorer/edge Reconnu !
+            </h2>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Vous utilisez actuellement une version d'Internet Explorer/Edge.
+            Malheuresement, ce navigateur ne supporte pas notre application :( .
+          </p>
+
+          <p>
+            {" "}
+            Veuillez utiliser un navigateur plus performant (Mozilla
+            Firefox/Google Chrome) afin de continuer. Merci de votre
+            compréhension.
+          </p>
+        </Modal.Body>
+      </Modal>,
+      document.getElementById("refgpec")
+    );
+  } else {
+    return ReactDOM.render(
+      <RefGpecHome
+        levelsModel={levelsModel}
+        skillsModel={skillsModel}
+        skillsTypesModel={skillsTypesModel}
+        skillsDomainsModel={skillsDomainsModel}
+        profilsModel={profilsModel}
+        profilsSkillsModel={profilsSkillsModel}
+      />,
+      document.getElementById("refgpec")
+    );
+  }
+}
+
+function detectIE() {
+  let ua = window.navigator.userAgent;
+  let msie = ua.indexOf("MSIE ");
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+  }
+
+  let trident = ua.indexOf("Trident/");
+  if (trident > 0) {
+    // IE 11 => return version number
+    let rv = ua.indexOf("rv:");
+    return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+  }
+
+  let edge = ua.indexOf("Edge/");
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+  }
+
+  // other browser
+  return false;
+}
 $(function() {
   render();
 });
